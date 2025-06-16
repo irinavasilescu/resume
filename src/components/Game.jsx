3dad007959cf6de046bdfe4552d16d212a062de5
Irina Vasilescu <irinaavasilescu@gmail.com>
2025-06-16 23:10:10 +0300

    add title

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,6 +4,13 @@ import './Game.css';
 const Game = () => {
   const [position, setPosition] = useState(0);
   const [isJumping, setIsJumping] = useState(false);
+  const [clouds, setClouds] = useState([
+    { id: 1, top: '50px', left: '100px' },
+    { id: 4, top: '90px', left: '300px' },
+    { id: 2, top: '50px', left: '500px' },
+    { id: 3, top: '80px', left: '800px' },
+    { id: 5, top: '20px', left: '1000px' },
+  ]);
 
   useEffect(() => {
     const handleKeyDown = (event) => {
@@ -25,11 +32,26 @@ const Game = () => {
 
   return (
     <div className="game-container">
+      <div className="game-title">
+        A frontend developer's <br/> adventures in the wild
+      </div>
+      {clouds.map(cloud => (
+        <div
+          key={cloud.id}
+          className="cloud"
+          style={{ top: cloud.top, left: cloud.left }}
+        ></div>
+      ))}
+
+      <div
+        className="building"
+        style={{ left: '300px' }}
+      ></div>
+
       <div 
         className={`character ${isJumping ? 'jump' : ''}`}
         style={{ left: `${position}px` }}
       >
-        
       </div>
       <div className="ground"></div>
     </div>
